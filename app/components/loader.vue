247f36d6a5daa2dd6d415971e3a126818e98c615
ArSiJa8 <silvan@arsija.net>
2026-07-13 20:55:17 +0200

    add test loader component and add it to main page as test

---
+++ app/components/loader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="loader"></div>
+</template>
+
+<style scoped>
+.loader {
+  border: 16px solid #1c6ade;
+  border-top: 16px solid #fff;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  will-change: transform;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
